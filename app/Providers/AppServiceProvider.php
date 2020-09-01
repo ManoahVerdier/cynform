c63c149f7ce9f2a2418bcd63f8cdd5d0d4749452
Manoah VERDIER <verdier.developpement@gmail.com>
2020-09-01 08:24:28 +0200

    Debug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Categorie;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +25,19 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if (Schema::hasTable('formations')) {
+            $categories = Categorie::distinct('nom')
+                ->orderBy('nom', 'ASC')
+                ->get();
+            /*$conseils_header = Conseil::distinct('certification')
+                ->orderBy('certification', 'ASC')
+                ->get();
+            $hp = Homepage::first();*/
+
+            View::share('categories', $categories);
+            /*View::share('conseils_header', $conseils_header);
+            View::share('hp', $hp);*/
+        }
     }
 }
