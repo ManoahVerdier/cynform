7f1e01b845390a8824f7117f49e3882793aea08c
Manoah VERDIER <verdier.developpement@gmail.com>
2021-01-22 16:22:14 +0100

    speed opt

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,7 @@
 
         {{-- JavaScript --}}
         @section('script')
-        {!! Html::script(mix('/js/app.js')) !!}
+            <script src={{mix('/js/app.js')}} async></script>
         @show
 
     </body>
