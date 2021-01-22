d6c875142dc8b4890a7c86f9786e1b397a445567
Manoah VERDIER <verdier.developpement@gmail.com>
2021-01-22 18:09:34 +0100

    test css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,10 +23,7 @@
         <title>@yield('title')</title>
         <meta name="description" content="@yield('description')"/>
 
-        {{-- CSS --}}
-        @section('css')
-        {!! Html::style(mix('/css/app.css')) !!}
-        @show
+        
 
         @yield('captcha')
 
@@ -63,5 +60,10 @@
             <script src={{mix('/js/app.js')}} async></script>
         @show
         <script src="https://kit.fontawesome.com/4236d13d2d.js" crossorigin="anonymous"></script>
+
+        {{-- CSS --}}
+        @section('css')
+        {!! Html::style(mix('/css/app.css')) !!}
+        @show
     </body>
 </html>
