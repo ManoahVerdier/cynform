3977edcd97b0d7c84160d6e7b91c2284f0f73cc8
ManoahVerdier <verdier.developpement@gmail.com>
2022-03-29 12:40:31 +0200

    Update app.blade.php
    Suppression analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,19 +31,19 @@
         @yield('captcha')
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-9998670-20"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-9998670-20');
+        <!--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-9998670-20"></script>-->
+        <!--<script>-->
+        <!--    window.dataLayer = window.dataLayer || [];-->
+        <!--    function gtag(){dataLayer.push(arguments);}-->
+        <!--    gtag('js', new Date());-->
+        <!--    gtag('config', 'UA-9998670-20');-->
         </script>
     </head>
 
     <body @yield('body-attr') class="bg-white">
         <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV9DF45"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!--<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV9DF45"-->
+        <!--height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>-->
         <!-- End Google Tag Manager (noscript) -->
 
 
